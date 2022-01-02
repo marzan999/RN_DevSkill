fb91772446b541a983ce38ebea054544a4a96d63
Marzan Islam <55044750+marzan999@users.noreply.github.com>
2022-01-02 19:49:00 +0600

    planet data input

--- Planet-App/src/components/text/text.js
+++ Planet-App/src/components/text/text.js
@@ -1,12 +1,15 @@
 import React from 'react'
-import { View, Text as RNText } from 'react-native'
-import { mergeAll, flatten } from 'ramda'
+import { View, Text as RNText, StyleSheet } from 'react-native'
+//import { mergeAll, flatten } from 'ramda'
 import { presets } from './text.preset'
 
 export default function Text({ children, preset = 'default', style }) {
-    const textStyles = mergeAll(
-        flatten([presets[preset] || presets.default, style])
-    )
+    // const textStyles = mergeAll(
+    //     flatten([presets[preset] || presets.default, style])
+    // )
+
+    const textStyles = StyleSheet.compose(presets[preset], style)
+
     return (
         <View>
             <RNText style={textStyles}>
