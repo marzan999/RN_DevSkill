8351437ad2cd6cd49a8dcc5d1313b9af2298dd07
Marzan Islam <55044750+marzan999@users.noreply.github.com>
2022-01-03 20:46:55 +0600

    add image for all planet

--- Planet-App/src/components/Planet-header.js
+++ Planet-App/src/components/Planet-header.js
@@ -1,17 +1,28 @@
 import React from 'react'
-import { View, TouchableOpacity } from 'react-native'
+import { View, TouchableOpacity, StyleSheet } from 'react-native'
 import Text from './text/text'
 import { spacing } from '../theme/spacing'
 import { colors } from '../theme/color'
 import { AntDesign } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
 
+const styles = StyleSheet.create({
+    container: {
+        paddingLeft: spacing[6],
+        paddingVertical: spacing[4],
+        borderBottomWidth: 0.5,
+        borderColor: colors.white,
+        flexDirection: 'row',
+        alignItems: 'center'
+    }
+})
+
 export default function PlanetHeader({ backButton = false }) {
 
     const navigation = useNavigation();
 
     return (
-        <View style={{ paddingLeft: spacing[6], paddingVertical: spacing[4], borderBottomWidth: 0.5, borderColor: colors.white, flexDirection: 'row', alignItems: 'center' }}>
+        <View style={styles.container}>
             {backButton &&
                 <TouchableOpacity onPress={() => navigation.goBack()}>
                     <AntDesign name="left" size={24} color="white" style={{ marginRight: spacing[4], top: 2 }} />
--- Planet-App/src/screens/Details.js
+++ Planet-App/src/screens/Details.js
@@ -1,17 +1,24 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, ScrollView, Image } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import PlanetHeader from '../components/Planet-header'
 import Text from '../components/text/text'
 import { colors } from '../theme/color'
+import { spacing } from '../theme/spacing'
 
 export default function Details({ route }) {
 
     const { planet } = route.params;
+    const { surfaceImage } = planet
 
     return (
         <SafeAreaView style={{ backgroundColor: colors.black, flex: 1 }}>
             <PlanetHeader backButton={true} />
+            <ScrollView>
+                <View style={{ paddingVertical: spacing[5] }}>
+                    <Image source={surfaceImage} style={{ alignSelf: 'center' }} />
+                </View>
+            </ScrollView>
         </SafeAreaView>
     )
 }
