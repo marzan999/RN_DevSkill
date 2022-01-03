65e41038ddc832761aca1f6315cd63ed7dac5f13
Marzan Islam <55044750+marzan999@users.noreply.github.com>
2022-01-04 01:20:42 +0600

    add wikipedia link

--- Planet-App/src/screens/Details.js
+++ Planet-App/src/screens/Details.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, ScrollView, Image } from 'react-native'
+import { View, ScrollView, Image, Linking, Pressable } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import PlanetHeader from '../components/Planet-header'
 import Text from '../components/text/text'
@@ -9,21 +9,29 @@ import { spacing } from '../theme/spacing'
 export default function Details({ route }) {
 
     const { planet } = route.params;
-    const { surfaceImage, name, description } = planet
+    const { surfaceImage, name, description, wikiLink } = planet
 
     return (
         <SafeAreaView style={{ backgroundColor: colors.black, flex: 1 }}>
             <PlanetHeader backButton={true} />
             <ScrollView>
-                <View style={{ paddingVertical: spacing[5] }}>
-                    <Image source={surfaceImage} style={{ alignSelf: 'center' }} />
-                    <View>
-                        <Text>
+                <View style={{ padding: spacing[5], alignItems: 'center', justifyContent: 'center' }}>
+                    <Image source={surfaceImage} style={{ alignSelf: 'center', height: 250, }} resizeMode='contain' />
+                    <View style={{ marginTop: spacing[8] }}>
+                        <Text preset='h1' style={{ textAlign: 'center', textTransform: 'uppercase' }}>
                             {name}
                         </Text>
-                        <Text>
+                        <Text style={{ marginTop: spacing[5], textAlign: 'center', paddingHorizontal: spacing[4], lineHeight: spacing[5] }}>
                             {description}
                         </Text>
+                    </View>
+
+                    <View style={{ flexDirection: 'row', marginTop: spacing[8] }}>
+                        <Text>Source: </Text>
+                        <Pressable onPress={() => Linking.openURL(wikiLink)}>
+                            <Text style={{ textDecorationLine: 'underline' }}>Wikipedia</Text>
+                        </Pressable>
+
                     </View>
                 </View>
             </ScrollView>
